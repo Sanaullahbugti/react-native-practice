66efd93ec3b7eb2f63ec511ab0a46bea7bbec507
Sanaullahbugti <sanaullahbugti821@gmail.com>
2019-07-04 02:34:17 +0500

    List Example added

--- components/ScrollableListExample.js
+++ components/ScrollableListExample.js
@@ -2,39 +2,39 @@ import React, { Component } from "react";
 import { ScrollView } from "react-native";
 import { Text } from "native-base";
 class ScrollableListExample extends Component {
-  names = [
-    { name: "ali" },
-    { name: "bugti" },
-    { name: "Shakeel" },
-    { name: "fahad" },
-    { name: "parbhu" },
-    { name: "darshan" },
-    { name: "owais" },
-    { name: "sanju" },
-    { name: "ali" },
-    { name: "ali" },
-    { name: "ali" },
-    { name: "ali" },
-    { name: "bugti" },
-    { name: "Shakeel" },
-    { name: "fahad" },
-    { name: "parbhu" },
-    { name: "darshan" },
-    { name: "owais" },
-    { name: "sanju" },
-    { name: "ali" },
-    { name: "ali" },
-    { name: "ali" }
-  ];
-  render() {
-    return (
-      <ScrollView style={{ backgroundColor: "white" }}>
-        {this.names.map(person => (
-          <Text>{person.name}</Text>
-        ))}
-      </ScrollView>
-    );
-  }
+	names = [
+		{ name: "ali" },
+		{ name: "bugti" },
+		{ name: "Shakeel" },
+		{ name: "fahad" },
+		{ name: "parbhu" },
+		{ name: "darshan" },
+		{ name: "owais" },
+		{ name: "sanju" },
+		{ name: "ali" },
+		{ name: "ali" },
+		{ name: "ali" },
+		{ name: "ali" },
+		{ name: "bugti" },
+		{ name: "Shakeel" },
+		{ name: "fahad" },
+		{ name: "parbhu" },
+		{ name: "darshan" },
+		{ name: "owais" },
+		{ name: "sanju" },
+		{ name: "ali" },
+		{ name: "ali" },
+		{ name: "ali" }
+	];
+	render() {
+		return (
+			<ScrollView style={{ backgroundColor: "white" }}>
+				{this.names.map(person => (
+					<Text>{person.name}</Text>
+				))}
+			</ScrollView>
+		);
+	}
 }
 
 export default ScrollableListExample;
