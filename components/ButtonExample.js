0a00e3b360e2a6ef2639baf4d8a3a8cc76601aa8
Sanaullahbugti <sanaullahbugti821@gmail.com>
2019-07-08 02:32:23 +0500

    Button example added

--- components/ButtonExample.js
+++ components/ButtonExample.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Button  } from "react-native";
+import { Button } from "react-native";
 export default class ButtonExmple extends Component {
-    handlePress =() =>{
-        alert("button clicked!!!")
-    }
-  render() {
-    return <Button title="Click me!!" onPress ={this.handlePress} />;
-  }
+	handlePress = () => {
+		alert("button clicked!!!")
+	}
+	render() {
+		return <Button title="Click me!!" onPress={this.handlePress} />;
+	}
 }
