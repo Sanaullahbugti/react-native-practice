2077127563751338d44ce7756cd64b3158d8c7f4
Sanaullah <sanaullahbugti821@gail.com>
2019-07-11 18:52:30 +0500

    Date picker from native base

--- App.js
+++ App.js
@@ -1,47 +1,10 @@
-import React from "react";
-import ButtonExmple from "./components/ButtonExample";
-import SliderExample from "./components/SliderExample";
-import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
-import TouchableOpacityExample from "./components/TouchableOpacityExample";
-import ScrollableListExample from "./components/ScrollableListExample";
-import CameraExample from "./components/CameraExample";
-import LoadingImage from "./components/LoadingImage";
-export default class App extends React.Component {
-  state = {
-    num: 0
-  };
+import React, { Component } from "react";
+import DatePickerExample from "./components/DatePickerExample";
 
-  handleValueChange = value => {
-    this.setState({
-      num: value
-    });
-  };
-  handleLongPress = () => {
-    alert("long Press");
-  };
-  handlePress = () => {
-    alert("only press");
-  };
+
+class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <View style={{ backgroundColor: "blue", flex: 2 }}>
-          <CameraExample />
-        </View>
-        <View style={{ backgroundColor: "yellow", flex: 3 }}>
-          <SliderExample />
-        </View>
-        {/* <ButtonExmple /> */}
-        <LoadingImage/>
-        {/* <TouchableOpacityExample /> */}
-      </View>
-    );
+    return <DatePickerExample />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "grey"
-  }
-});
+export default App;
